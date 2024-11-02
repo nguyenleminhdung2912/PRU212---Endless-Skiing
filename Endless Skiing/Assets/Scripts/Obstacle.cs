5135eb4543083729ebc5ee81f723bd13de762f56
Bill <khoibtvn03@gmail.com>
2024-11-02 12:21:56 +0700

    Add sound effect

--- Endless Skiing/Assets/Scripts/Obstacle.cs
+++ Endless Skiing/Assets/Scripts/Obstacle.cs
@@ -5,16 +5,19 @@ using System.Collections.Generic;
 public class Obstacle : MonoBehaviour {
 
     PlayerMovement playerMovement;
+    [SerializeField] AudioClip deathSound; 
 
-	private void Start () {
+    private void Start () {
         playerMovement = GameObject.FindObjectOfType<PlayerMovement>();
-	}
+    }
 
     private void OnCollisionEnter (Collision collision)
     {
         if (collision.gameObject.name == "Player") {
+            
             // Kill the player
             playerMovement.Die();
+            AudioSource.PlayClipAtPoint(deathSound, transform.position);
         }
     }
 
